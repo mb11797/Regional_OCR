f65b1cfea88506be251146c4dd4f03808e59bee6
mb11797 <mb.manasbhardwaj@gmail.com>
2018-06-22 12:48:42 +0530

    binary file reading

--- tesseract_eng_model.py
+++ tesseract_eng_model.py
@@ -1,17 +1,71 @@
-import pickle
-import _pickle as cPickle
+import io
+import os
+import codecs
 
-def unpickle(file):
-    print(file)
-    fo = open(file, 'rb')
-    dict = cPickle.load(fo)
-    fo.close()
-    return dict
+def readFile(path):
+#    inBody = False
+#    lines = []
+#    f = io.open(path, 'r', encoding = 'latin1')
+##    f = io.open(path, 'r', encoding='base64')
+#    for line in f:
+#        if inBody:
+#            lines.append(line)
+#        elif line == '\n':
+#            inBody = True
+#    f.close()
+    f = codecs.open(path, 'rb')
+    lines = f.read()
+    print(lines)
+#    lines = pickle.load('path')
+    return lines
+    
+data = readFile('tesseract_eng.traineddata')
 
-d = unpickle("tesseract_eng_traineddata")
+    
     
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#import pickle
+##import _pickle as cPickle
+#
+#def unpickle(file):
+#    print(file)
+#    fo = open(file, 'rb')
+##    dict = cPickle.load(fo)
+#    data = pickle.load(fo, encoding='bytes')
+#    fo.close()
+#    return dict
+#
+##d = unpickle("tesseract_eng_traineddata")
+#    
+#eng_model = unpickle('tesseract_eng_traineddata')
+#
+#
+#
+#
 
 
